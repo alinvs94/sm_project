7bb23f0aae6c4273239cee8114eabd426098f05a
alinvs94 <alin.vasiliu94@gmail.com>
2023-08-11 00:57:04 +0300

    token check

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import { AuthPage } from "./pages/AuthPage";
 import { Layout } from "./components/Layout";
 import { UserPage } from "./pages/UserPage";
 import { AppData } from "./components/AppData/AppData";
+import axios from "axios";
+import CheckLogin from "./components/CheckLogin/CheckLogin";
+
+axios.defaults.baseURL = "http://127.0.0.1:8000/api";
+if (localStorage.getItem("tk") !== undefined) {
+   axios.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
+      "__tk"
+   )}`;
+} else {
+   delete axios.defaults.headers["Authorization"];
+}
 
 function App() {
    return (
       <AppData>
          <Layout>
-            <Routes>
-               <Route path="/home" element={<HomePage></HomePage>}></Route>
-               <Route path="/auth" element={<AuthPage></AuthPage>}></Route>
-               <Route path="/user" element={<UserPage></UserPage>}></Route>
-            </Routes>
+            <CheckLogin>
+               <Routes>
+                  <Route path="/home" element={<HomePage></HomePage>}></Route>
+                  <Route path="/user" element={<UserPage></UserPage>}></Route>
+                  <Route path="/auth" element={<AuthPage></AuthPage>}></Route>
+               </Routes>
+            </CheckLogin>
          </Layout>
       </AppData>
    );
