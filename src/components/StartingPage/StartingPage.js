2a93089be29ad6f105b1d24aa1e867ff09ad0977
alinvs94 <alin.vasiliu94@gmail.com>
2023-08-11 00:59:20 +0300

    prettier

--- src/components/StartingPage/StartingPage.js
+++ src/components/StartingPage/StartingPage.js
@@ -2,41 +2,42 @@ import { LeftContent } from "./LeftContent/LeftContent";
 import { NewsFeed } from "./NewsFeed/NewsFeed";
 import { RightContent } from "./RightContent/RightContent";
 import styles from "./StartingPage.module.scss";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { DataContext } from "../AppData/AppData";
 
 export function StartingPage() {
-  const { USERS, posts, numbArray } = useContext(DataContext);
+   const { USERS, posts, numbArray } = useContext(DataContext);
 
-  return (
-    <section className={styles.mainContainer}>
-      <aside className={styles.leftContent}>
-        <LeftContent></LeftContent>
-      </aside>
+   return (
+      <section className={styles.mainContainer}>
+         <aside className={styles.leftContent}>
+            <LeftContent></LeftContent>
+         </aside>
 
-      <div className={styles.newsFeed}>
-        {posts.map((post) => {
-          if (post.id < 31) {
-            let postUser = USERS.find((user) => user.userId === numbArray[post.id]);
-            if (postUser !== undefined) {
+         <div className={styles.newsFeed}>
+            {posts.map((post) => {
+               if (post.id < 31) {
+                  let postUser = USERS.find(
+                     (user) => user.userId === numbArray[post.id]
+                  );
+                  if (postUser !== undefined) {
+                     return (
+                        <NewsFeed
+                           postData={post}
+                           key={post.id}
+                           id={post.id}
+                           user={postUser}
+                        ></NewsFeed>
+                     );
+                  }
+               }
+            })}
+            ;
+         </div>
 
-              return (
-                <NewsFeed
-                  postData={post}
-                  key={post.id}
-                  id={post.id}
-                  user={postUser}
-                ></NewsFeed>
-              );
-            }
-          }
-        })}
-        ;
-      </div>
-
-      <aside className={styles.rightContent}>
-        <RightContent></RightContent>
-      </aside>
-    </section>
-  );
+         <aside className={styles.rightContent}>
+            <RightContent></RightContent>
+         </aside>
+      </section>
+   );
 }
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -2,9 +2,8 @@ import { ProfilePage } from "../components/ProfilePage/ProfilePage";
 import { DataContext } from "../components/AppData/AppData";
 import { useContext } from "react";
 
-
 export function UserPage() {
-   const {handleUser} = useContext(DataContext);
+   const { handleUser } = useContext(DataContext);
 
-   return <ProfilePage></ProfilePage>
-};
+   return <ProfilePage></ProfilePage>;
+}
