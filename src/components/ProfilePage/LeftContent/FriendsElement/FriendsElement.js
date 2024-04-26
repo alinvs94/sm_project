34ba434856cb24fd553172adc5b713f648f9c870
alinvs94 <alin.vasiliu94@gmail.com>
2024-04-26 22:06:03 +0300

    working friend element

--- src/components/ProfilePage/LeftContent/FriendsElement/FriendsElement.js
+++ src/components/ProfilePage/LeftContent/FriendsElement/FriendsElement.js
@@ -1,8 +1,18 @@
-import styles from './FriendsElement.module.scss'
+import { useContext } from "react";
+import { DataContext } from "../../../AppData/AppData";
+import styles from "./FriendsElement.module.scss";
 
-export function FriendsElement(props) {
-   return <div className={styles.friendsElement}>
-      <img src={props.picture}></img>
-      <p>{props.name}</p>
-   </div>
-};
+export function FriendsElement({props, friend}) {
+   const { handleUser } = useContext(DataContext);
+   return (
+      <div
+         className={styles.friendsElement}
+         onClick={(e) => {
+            handleUser(friend);
+         }}
+      >
+         <img src={friend.picture}></img>
+         <p>{friend.name}</p>
+      </div>
+   );
+}
